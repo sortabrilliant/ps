760a48722c577b9b79fb1472f44a05eb1f498e09
George Mamadashvili <georgemamadashvili@gmail.com>
2020-01-15 13:26:05 +0400

    Adds different state for button close
    See: #2.

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,12 @@
 jQuery( document ).ready( function( $ ) {
-	$( '.wp-block-post-script__button' ).click( function() {
+	$button   = $( '.wp-block-post-script__button' );
+	$btnLabel = $button.text();
+	
+	$button.click( function() {
 		$( this ).parent().toggleClass( 'is-active' );
+
+		$( this ).html( function( i, html ) {
+			return html === $btnLabel ? '&times;' : $btnLabel;
+		} );
 	} );
 } );
